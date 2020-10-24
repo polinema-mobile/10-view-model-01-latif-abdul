b1b26f7a566e54c162559adaf5fc2be950e5e705
Putra Prima Arhandi <putraprima@gmail.com>
2020-10-25 05:18:21 +0700

    View Model and Live Data

--- app/src/main/java/id/putraprima/courtcounter/fragments/CounterFragment.java
+++ app/src/main/java/id/putraprima/courtcounter/fragments/CounterFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.util.Log;
@@ -13,6 +14,7 @@ import android.view.ViewGroup;
 
 import id.putraprima.courtcounter.R;
 import id.putraprima.courtcounter.databinding.FragmentCounterBinding;
+import id.putraprima.courtcounter.models.Score;
 import id.putraprima.courtcounter.viewmodels.CounterViewModels;
 
 public class CounterFragment extends Fragment {
@@ -31,7 +33,13 @@ public class CounterFragment extends Fragment {
         mViewModels = new ViewModelProvider(this).get(CounterViewModels.class);
         binding.setLifecycleOwner(this);
         binding.setCounterViewModel(mViewModels);
+        mViewModels.scoreMutableLiveData.observe(getViewLifecycleOwner(), new Observer<Score>() {
+            @Override
+            public void onChanged(Score score) {
+                binding.txtScoreHome.setText(Integer.toString(score.getHomeScore()));
+                binding.txtScoreAway.setText(Integer.toString(score.getAwayScore()));
+            }
+        });
         return binding.getRoot();
-
     }
 }
--- app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
+++ app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
@@ -2,21 +2,25 @@ package id.putraprima.courtcounter.viewmodels;
 
 import android.util.Log;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import id.putraprima.courtcounter.models.Score;
 
 public class CounterViewModels extends ViewModel {
 
+    public MutableLiveData<Score> scoreMutableLiveData = new MutableLiveData<>();
     public Score score = new Score(0,0);
 
     public void addPointHome(int point){
         score.setHomeScore(score.getHomeScore()+point);
+        scoreMutableLiveData.setValue(score);
         Log.d("Point Home : ", String.valueOf(score.getHomeScore()));
     }
 
     public void addPointAway(int point){
         score.setAwayScore(score.getAwayScore()+point);
+        scoreMutableLiveData.setValue(score);
         Log.d("Point Away : ", String.valueOf(score.getAwayScore()));
     }
 }
