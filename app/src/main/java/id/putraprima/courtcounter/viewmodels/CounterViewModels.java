0e24ce95db8f1dd6bac48bdeb999a9f8e3298c97
Putra Prima Arhandi <putraprima@gmail.com>
2020-10-25 05:06:46 +0700

    Better View Model Code But No Live Data

--- app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
+++ app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
@@ -1,9 +1,22 @@
 package id.putraprima.courtcounter.viewmodels;
 
+import android.util.Log;
+
 import androidx.lifecycle.ViewModel;
 
 import id.putraprima.courtcounter.models.Score;
 
 public class CounterViewModels extends ViewModel {
+
     public Score score = new Score(0,0);
+
+    public void addPointHome(int point){
+        score.setHomeScore(score.getHomeScore()+point);
+        Log.d("Point Home : ", String.valueOf(score.getHomeScore()));
+    }
+
+    public void addPointAway(int point){
+        score.setAwayScore(score.getAwayScore()+point);
+        Log.d("Point Away : ", String.valueOf(score.getAwayScore()));
+    }
 }
